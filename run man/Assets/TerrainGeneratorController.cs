f48bf5047fee4da3b9828fcf232f184839eb99dd
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-06-17 16:47:39 +0700

    Terrain Object Pooling
    - add and initialise variable pool Dictionary
    - add function GenerateFromPool and
    ReturnToPool
    - change Instantiate statement with
    GenerateFromPool
    - change Destroy statement with ReturnToPool

--- run man/Assets/TerrainGeneratorController.cs
+++ run man/Assets/TerrainGeneratorController.cs
@@ -39,10 +39,15 @@ public class TerrainGeneratorController : MonoBehaviour
     [Header("Force Early Template")]
     public List<TerrainTemplateController> earlyTerrainTemplates;
     private float lastRemovedPositionX;
+    private Dictionary<string, List<GameObject>> pool;
+    // Start is called before the first frame update
     void Start()
     {
+        // init pool
+        pool = new Dictionary<string, List<GameObject>>();
         spawnedTerrain = new List<GameObject>();
         lastGeneratedPositionX = GetHorizontalPositionStart();
+        lastRemovedPositionX = lastGeneratedPositionX - terrainTemplateWidth;
         foreach (TerrainTemplateController terrain in earlyTerrainTemplates)
         {
             GenerateTerrain(lastGeneratedPositionX, terrain);
@@ -54,6 +59,41 @@ public class TerrainGeneratorController : MonoBehaviour
             lastGeneratedPositionX += terrainTemplateWidth;
         }
     }
+
+    private GameObject GenerateFromPool(GameObject item, Transform parent)
+    {
+        if (pool.ContainsKey(item.name))
+        {
+            // if item available in pool
+            if (pool[item.name].Count > 0)
+            {
+                GameObject newItemFromPool = pool[item.name][0];
+                pool[item.name].Remove(newItemFromPool);
+                newItemFromPool.SetActive(true);
+                return newItemFromPool;
+            }
+        }
+        else
+        {
+            // if item list not defined, create new one
+            pool.Add(item.name, new List<GameObject>());
+        }
+        // create new one if no item available in pool
+        GameObject newItem = Instantiate(item, parent);
+
+        newItem.name = item.name;
+        return newItem;
+    }
+    private void ReturnToPool(GameObject item)
+    {
+        if (!pool.ContainsKey(item.name))
+        {
+            Debug.LogError("INVALID POOL ITEM!!");
+        }
+        pool[item.name].Add(item);
+        item.SetActive(false);
+    }
+
     private void GenerateTerrain(float posX, TerrainTemplateController forceterrain =
 null)
     {
@@ -68,7 +108,8 @@ null)
         {
             item = forceterrain.gameObject;
         }
-        GameObject newTerrain = Instantiate(item, transform);
+        //GameObject newTerrain = Instantiate(item, transform);
+        GameObject newTerrain = GenerateFromPool(item, transform);
         newTerrain.transform.position = new Vector2(posX, 0f);
         spawnedTerrain.Add(newTerrain);
     }
@@ -104,7 +145,9 @@ null)
         if (terrainToRemove != null)
         {
             spawnedTerrain.Remove(terrainToRemove);
-            Destroy(terrainToRemove);
+            //Destroy(terrainToRemove);
+            ReturnToPool(terrainToRemove);
         }
     }
+
 }
