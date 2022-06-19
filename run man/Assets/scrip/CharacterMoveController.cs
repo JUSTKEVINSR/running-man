a82661aa0e3c4f39924d818e2a9b2aaebbf1e027
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-06-19 18:44:13 +0700

    ok it runs but short of
    it runs but the posision is weird still need fixing to do

---
+++ run man/Assets/scrip/CharacterMoveController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+    private Rigidbody2D rig;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+    public float jumpAmount = 10;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    private Animator anim;
+    private CharacterSoundController sound;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    void FixedUpdate()
+    {
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+        groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        // calculate velocity vector
+        Vector2 velocityVector = rig.velocity;
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
+        0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
+        groundRaycastDistance), Color.white);
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            {
+                rig.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+                sound.PlayJump();
+            }
+        }
+        // change animation
+        anim.SetBool("isOnGround", isOnGround);
+    }
+}
